1a82260dc8e77df4ab5d68876e03e1aa72f04c2c
sinchang <sinchangwen@gmail.com>
2023-02-19 18:27:27 +0800

    Update umami

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Toaster } from 'react-hot-toast'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy='afterInteractive'
+        data-website-id='afa609f0-7988-4c16-93d2-7b85efbf0dc2'
+        src='https://umami-production-af5b.up.railway.app/umami.js'
+      />
       <Component {...pageProps} />
       <Toaster />
     </>
